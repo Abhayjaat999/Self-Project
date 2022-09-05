d66b18bcc9980ceb3fcd1952419cbaf19d86524c
deepak9664 <singhdepak30@gmail.com>
2022-09-05 17:25:09 +0530

    project

---
+++ src/models/blogmodel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const BlogSchema = new mongoose.Schema({
+    title: {
+        type:String,
+        required:true
+    },
+    body:  {
+        type:String,
+        required:true
+ },
+ authorId:  {
+    type: ObjectId,
+    ref: "Author",
+    required:true
+},
+   tags: [String] ,
+   category: {type:String, 
+    required:true, 
+    enum: ["technology", "entertainment", "life style", "food"," fashion"],
+   },
+   subcategory: {type:[String], 
+    enum:["technology"-["web development","mobile development","AI","ML", "etc"]] 
+},
+isDeleted: {type:Boolean, default: false},
+isPublished: {type:Boolean, default: false},
+}, { timestamps: true });
+module.exports = mongoose.model('Blog', BlogSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+const Authorcontroller= require("../controllers/authorcontroller")
+
+router.post("/authors", Authorcontroller.createAuthors  )
+
 
 module.exports = router;
