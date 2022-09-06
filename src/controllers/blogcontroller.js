ec76a09dba5c315abd7b7bce590b5d281924069c
sushant bhaiswar <sb.bhaiswar@gmail.com>
2022-09-07 02:53:40 +0530

    updated

--- src/controllers/blogcontroller.js
+++ src/controllers/blogcontroller.js
@@ -37,11 +37,11 @@ const updateblog = async function (req, res) {
     }
 };
 
-// const isValid = (val) => {
-//     if (typeof val === "undefined" || val === "null") return false;
-//     if (typeof val === "string" && val.trim().length === 0) return false
-//     return true;
-// }
+const isValid = (val) => {
+    if (typeof val === "undefined" || val === "null") return false;
+    if (typeof val === "string" && val.trim().length === 0) return false
+    return true;
+}
 const isvalidbody = (val) => {
     return Object.keys(val).length > 0
 }
--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,5 +1,1 @@
-const isValid = (val) => {
-    if (typeof val === "undefined" || val === "null") return false;
-    if (typeof val === "string" && val.trim().length === 0) return false
-    return true;
-}
+
