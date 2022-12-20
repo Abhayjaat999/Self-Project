fe1dc1867f888574847daaab71601668a840e604
Abhay999 <abhayjaat999@gmail.com>
2022-12-20 10:19:33 +0530

    first

---
+++ server.js
@@ -0,0 +1,19 @@
+const express=require('express')
+const app = express()
+const ejs=require('ejs')
+const path=require('path')
+const expressLayout= require('express-ejs-layouts')
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+app.use(expressLayout)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
+
+
+
+app.listen(process.env.PORT || 3000 ,()=>{
+    console.log(`Runing port ${process.env.PORT || 3000}`);
+})
